45ed0d87b394e1432af9ac4f58a5028bc596a8be
hemanth-gopal <hemanth2396@gmail.com>
2022-07-07 01:19:18 -0700

    final

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func formHandler(w http.ResponseWriter,r *http.Request){
 	}
 	fmt.Fprintf(w,"POST request successfull")
 	name := r.FormValue("name")
-	address := r.FormValue("Agitddress")
+	address := r.FormValue("address")
 	fmt.Fprintf(w,"Name = %s\n",name)
 	fmt.Fprintf(w,"Address = %s\n",address)
 }
